b8670ab294cb9428c5482b91688645e6b215216b
Thabani <hlatshie01@gmail.com>
2024-05-17 01:12:18 +0200

    simple client server connectin established. Learning can resume and grow into buiding

---
+++ Main.java
@@ -0,0 +1,11 @@
+//package co.za.javaPlayground;
+//
+//import java.net.*;
+//import java.io.*;
+//
+//class Main {
+//    public static void main(String[] args) throws IOException {
+//        server server = new server(1234);
+//        server.start(); // Start the server on port 1234
+//    }
+//}
---
+++ client.java
@@ -0,0 +1,25 @@
+package co.za.javaPlayground;
+
+import java.net.*;
+import java.io.*;
+
+public class client {
+
+    public static void main(String args[]) throws IOException {
+//        try (
+                Socket socket = new Socket("localhost", 1234);
+//                PrintStream out = new PrintStream(socket.getOutputStream());
+//                BufferedReader in = new BufferedReader(new InputStreamReader(
+//                        socket.getInputStream()));
+////        )
+//        {
+//            out.println("Hello WeThinkCode");
+//            out.flush();
+//            String messageFromServer = in.readLine();
+//            System.out.println("Response: "+messageFromServer);
+//        }
+//        catch (IOException e) {
+//            e.printStackTrace();
+        }
+    }
+//}
